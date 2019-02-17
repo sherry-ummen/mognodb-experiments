efa5b2a130fb6af1b6abf0084dc3d64e30e8dce5
Sherry Ummen <sherry.ummen@gmail.com>
2019-02-17 19:13:10 +0200

    Transactions experiment

--- csharp/dotnetcore/SpeedTest/Program.cs
+++ csharp/dotnetcore/SpeedTest/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Diagnostics;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
 
 namespace SpeedTest
 {
     class Program
     {
+
+        class NDocument{
+            public string Name { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var stopwatch = new Stopwatch();
+
+            var client = new MongoClient();
+            var database = client.GetDatabase("foo");
+            var collection = database.GetCollection<NDocument>("bar");
+            var session = client.StartSession();
+            stopwatch.Start();
+            session.StartTransaction(new TransactionOptions(
+                readConcern: ReadConcern.Snapshot,
+                writeConcern: WriteConcern.WMajority));
+
+            for (int i = 0; i < 10000; i++)
+            {
+                collection.InsertOne(session, new NDocument(){ Name = "index"+i });
+            }
+            session.CommitTransaction();
+            stopwatch.Stop();
+            Console.WriteLine($"Time taken:{stopwatch.ElapsedMilliseconds}");
         }
     }
 }
